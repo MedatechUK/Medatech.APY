43324794419bc3dd20e55b40c55272528797e742
Simon Barnett <simonbarnett@emerge-it.co.uk>
2022-06-12 15:43:57 +0100

    tests

---
+++ package/tests/infer_test.bat
@@ -0,0 +1,14 @@
+del gen1.py
+type gen1.json
+py makeprops.py gen1.json gen1.py -name order
+py gen1.py
+
+del gen2.py
+type gen2.json
+py makeprops.py gen2.json gen2.py -name order
+py gen2.py
+
+del gen3.py
+type gen3.json
+py makeprops.py gen3.json gen3.py -name order
+py gen3.py
